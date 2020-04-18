117f1d3be3f9550e1c1a40fad28456fa2e4f08ae
Syed Zaman Mostafiz <zamanwebdeveloper@gmail.com>
2020-04-18 17:29:18 +0600

    6/25 05. Templates Learning with Practice Program Completed.

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 @app.route('/index')
 # def index():
 #     return 'index'
 @app.route('/')
 def Home():
-    return 'Welcome to Home Page'
+    return render_template('home.html')
 @app.route('/product')
 def pro():
-    return 'This is the product page'
+    return render_template('product.html')
 # app.run(port=5002)
 app.run(port=5000, debug=True)
