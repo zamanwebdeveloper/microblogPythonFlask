dc5312aafc00b46d6ac4621b40157bb846eb3d04
Syed Zaman Mostafiz <zamanwebdeveloper@gmail.com>
2020-04-20 16:42:27 +0600

    8/25 07. Accessing dictionary variables Learning with Practice Program Completed.

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 @app.route('/index')
-# def index():
-#     return 'index'
 @app.route('/')
 def Home():
-    return render_template('home.html')
+    user = {'username': 'Syed Zaman Mostafiz'}
+    posts = [
+        {
+            'author':{'username': 'Jhon'},
+            'body':{'This is a beautiful day'}
+        },
+        {
+            'author': {'username': 'Jhon'},
+            'body': {"I'm Good Today"}
+        },
+        {
+            'author': {'username': 'Alice'},
+            'body': {"Got a new jog today"}
+        },
+    ]
+    return render_template('home.html',title='Home',user=user, posts=posts)
 @app.route('/product')
 def pro():
     return render_template('product.html')
