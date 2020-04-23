5cc6cb35aa5382e8ba3c9e275bef073df8d68b5e
Syed Zaman Mostafiz <zamanwebdeveloper@gmail.com>
2020-04-23 20:03:32 +0600

    10/25 09. Extending Base Template Learning with Practice Program Completed.

--- app.py
+++ app.py
@@ -1,26 +1,29 @@
 from flask import Flask, render_template
 app = Flask(__name__)
-@app.route('/index')
+@app.route('/home')
 @app.route('/')
 def Home():
     user = {'username': 'Syed Zaman Mostafiz'}
     posts = [
         {
             'author':{'username': 'Jhon'},
-            'body':{'This is a beautiful day'}
+            'body':'This is a beautiful day'
         },
         {
             'author': {'username': 'Jhon'},
-            'body': {"I'm Good Today"}
+            'body': "I'm Good Today"
         },
         {
             'author': {'username': 'Alice'},
-            'body': {"Got a new jog today"}
+            'body': "Got a new jog today"
         },
     ]
     return render_template('home.html',title='Home',user=user, posts=posts)
 @app.route('/product')
-def pro():
+def product():
     return render_template('product.html')
+@app.route('/about')
+def about():
+    return render_template('about.html')
 # app.run(port=5002)
 app.run(port=5000, debug=True)
